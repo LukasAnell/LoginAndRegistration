25203165f5d482888e5b6046a289b63dd9ecfc5f
LukasAnell <lukasmanell@gmail.com>
2023-11-16 09:26:10 -0800

    Implemented validation for name, username, password, and email.

--- app/src/main/java/com/example/loginandregistration/MainActivity.kt
+++ app/src/main/java/com/example/loginandregistration/MainActivity.kt
@@ -6,6 +6,8 @@ import android.os.Bundle
 import android.util.Log
 import android.widget.Button
 import android.widget.TextView
+import android.widget.Toast
+import androidx.core.widget.addTextChangedListener
 import com.example.loginandregistration.databinding.ActivityMainBinding
 
 class MainActivity: AppCompatActivity() {
@@ -15,19 +17,17 @@ class MainActivity: AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
 
-    private val EXTRA_USERNAME = ""
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
         setListeners()
 
-        val username = intent.getStringExtra(RegistrationActivity.username).toString() ?: ""
-        val password = intent.getStringExtra("password").toString() ?: ""
+        val username = intent.getStringExtra("username").toString()
+        val password = intent.getStringExtra("password").toString()
 
-        binding.textInputMainUsername.setText(username)
-        binding.textInputMainPassword.setText(password)
+        binding.textInputMainUsername.setText(if(username != "null") username else "")
+        binding.editTextMainPassword.setText(if(password != "null") password else "")
     }
 
     private fun setListeners() {
@@ -35,13 +35,27 @@ class MainActivity: AppCompatActivity() {
             Log.d(TAG, "setListeners: signUp clicked")
             val registerIntent = Intent(this, RegistrationActivity::class.java)
 
-            registerIntent.putExtra(EXTRA_USERNAME, binding.textInputMainUsername.text.toString())
+            registerIntent.putExtra("username", binding.textInputMainUsername.text.toString())
 
             startActivity(registerIntent)
         }
 
         binding.buttonMainLogin.setOnClickListener {
-
+//            if(RegistrationUtil.existingUsers.contains(binding.textInputMainUsername.text.toString())) {
+//                if(RegistrationUtil.validatePassword(binding.editTextMainPassword.text.toString(), binding.editTextMainPassword.text.toString())) {
+//                    Log.d(TAG, "setListeners: login clicked")
+//                    val homeIntent = Intent(this, HomeActivity::class.java)
+//
+//                    homeIntent.putExtra("username", binding.textInputMainUsername.text.toString())
+//                    homeIntent.putExtra("password", binding.editTextMainPassword.text.toString())
+//
+//                    startActivity(homeIntent)
+//                } else {
+//                    Toast.makeText(this, "Invalid password", Toast.LENGTH_SHORT).show()
+//                }
+//            } else {
+//                Toast.makeText(this, "Invalid username", Toast.LENGTH_SHORT).show()
+//            }
         }
     }
 }
--- app/src/main/java/com/example/loginandregistration/RegistrationActivity.kt
+++ app/src/main/java/com/example/loginandregistration/RegistrationActivity.kt
@@ -3,8 +3,12 @@ package com.example.loginandregistration
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
 import android.util.Log
 import android.widget.Button
+import android.widget.Toast
+import androidx.core.widget.addTextChangedListener
 import com.example.loginandregistration.databinding.ActivityRegistrationBinding
 
 class RegistrationActivity: AppCompatActivity() {
@@ -27,7 +31,7 @@ class RegistrationActivity: AppCompatActivity() {
 
         val username = intent.getStringExtra("username") ?: ""
 
-        binding.textInputRegistrationUsername.setText(username)
+        binding.textInputRegistrationUsername.setText(if(password != "null") username else "")
     }
 
     private fun setListeners() {
@@ -40,10 +44,72 @@ class RegistrationActivity: AppCompatActivity() {
             password = binding.editTextRegistrationPassword.text.toString()
             email = binding.textInputRegistrationEmail.text.toString()
 
+            RegistrationUtil.existingUsers = RegistrationUtil.existingUsers.plus(username)
+            Log.d(TAG, "existingUsers: ${RegistrationUtil.existingUsers}")
+
             loginIntent.putExtra("username", username)
             loginIntent.putExtra("password", password)
-
             startActivity(loginIntent)
         }
+
+        binding.textInputRegistrationUsername.addTextChangedListener(object: TextWatcher {
+            override fun afterTextChanged(s: Editable?) {
+                binding.buttonRegistrationRegister.isEnabled = RegistrationUtil.validateUsername(binding.textInputRegistrationUsername.text.toString())
+                Toast.makeText(this@RegistrationActivity, "Username must be at least 3 characters long and not already taken", Toast.LENGTH_SHORT).show()
+            }
+
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+                // do nothing
+            }
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                // do nothing
+            }
+        })
+
+        binding.editTextRegistrationPassword.addTextChangedListener(object: TextWatcher {
+            override fun afterTextChanged(s: Editable?) {
+                binding.buttonRegistrationRegister.isEnabled = RegistrationUtil.validatePassword(binding.editTextRegistrationPassword.text.toString(), binding.editTextRegistrationConfirmPassword.text.toString())
+                Toast.makeText(this@RegistrationActivity, "Password must be at least 8 characters long, contain at least one digit and one capital letter, and match the confirm password", Toast.LENGTH_SHORT).show()
+            }
+
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+                // do nothing
+            }
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                // do nothing
+            }
+        })
+
+        binding.textInputRegistrationName.addTextChangedListener(object: TextWatcher {
+            override fun afterTextChanged(s: Editable?) {
+                binding.buttonRegistrationRegister.isEnabled = RegistrationUtil.validateName(binding.textInputRegistrationName.text.toString())
+                Toast.makeText(this@RegistrationActivity, "Name must not be empty", Toast.LENGTH_SHORT).show()
+            }
+
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+                // do nothing
+            }
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                // do nothing
+            }
+        })
+
+        binding.textInputRegistrationEmail.addTextChangedListener(object: TextWatcher {
+            override fun afterTextChanged(s: Editable?) {
+                binding.buttonRegistrationRegister.isEnabled = RegistrationUtil.validateEmail(binding.textInputRegistrationEmail.text.toString())
+                Toast.makeText(this@RegistrationActivity, "Email must not be empty, must not be already taken, and must be in the proper email format", Toast.LENGTH_SHORT).show()
+            }
+
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+                // do nothing
+            }
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                // do nothing
+            }
+        })
     }
 }
